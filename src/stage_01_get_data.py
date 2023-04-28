49850738bef6d2a9f23a68e247adda1684b2992f
Abdulhakeem Omotolani Yaqoob <40827896+bhydemi@users.noreply.github.com>
2023-04-29 01:03:10 +0200

    cnn pipeline

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -4,12 +4,7 @@ import shutil
 from tqdm import tqdm
 import logging
 
-import sys
-
-sys.path.append("/path/to/src")
-
 import src.utils.common_utils as utils
-#from src.utils.common_utils import create_directories, read_yaml
 import random
 
 
@@ -24,29 +19,34 @@ logging.basicConfig(
 
 
 def get_data(config_path):
+    """This function downloads data from remote source and saves it in the local system
+    
+    Args:
+        config_path ([str]): [path to yaml file]
+    """
     config = utils.read_yaml(config_path)
-    remote_data_path = config["data_source"]
-    local_data_path = config["data_local"]
-    utils.create_directories([local_data_path])
-    for data_dir in tqdm(os.listdir(remote_data_path), colour="green"):
-        data_dir_path = os.path.join(remote_data_path, data_dir)
-        if os.path.isdir(data_dir_path):
-            for f in tqdm(os.listdir(data_dir_path)):
-                if f.endswith(".jpg"):
-                    shutil.copy(os.path.join(data_dir_path, f), local_data_path)
-    return local_data_path
+    
+    source_data_dirs = config["source_download_dirs"]
+    local_data_dirs = config["local_data_dirs"]
+
+    N = len(source_data_dirs)
+    for source_data_dir, local_data_dir in zip(source_data_dirs, local_data_dirs):
+        logging.info(f"Downloading data from {source_data_dir} to {local_data_dir}")
+        utils.create_directories([local_data_dir])
+        for file in tqdm(os.listdir(source_data_dir)):
+            shutil.copy(os.path.join(source_data_dir, file), local_data_dir)
+        logging.info(f"Downloaded data from {source_data_dir} to {local_data_dir}")
 
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
-    args.add_argument("--config", "-c", default="configs/config.yaml")
-    args.add_argument("--params", "-p", default="params.yaml")
+    args.add_argument("--config_path", "-c", default="configs/config.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info("\n********************")
         logging.info(f">>>>> stage {STAGE} started <<<<<")
-        get_data(config_path=parsed_args.config)
+        get_data(config_path=parsed_args.config_path)
         logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
     except Exception as e:
         logging.exception(e)
--- src/utils/common_utils.py
+++ src/utils/common_utils.py
@@ -4,19 +4,50 @@ import logging
 import time
 import pandas as pd
 import json
+import shutil
+import tqdm
 
 def read_yaml(path_to_yaml):
+    """This function reads the yaml file and returns the content
+    
+    Args:
+        path_to_yaml ([str]): [path to yaml file]
+    """
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
     logging.info(f'yaml file: {path_to_yaml} loaded successfully')
     return content
 
 def create_directories(path_to_directories: list):
+    """This function creates directories for the given path
+
+    Args:
+        path_to_directories (list): [list of paths to create directories]
+    """
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         logging.info(f"created directory at: {path}")
 
 def save_json(path: str, data: dict):
+    """This function saves the json file at the given path
+
+    Args:
+        path (str): [path to save the json file]
+        data (dict): [data to be saved in json file]
+    """
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
     logging.info(f"json file saved at: {path}")
+
+def copy_files(source: str, destination: str):
+    """This function copies files from source to destination
+
+    Args:
+        source (str): [source path]
+        destination (str): [destination path]
+    """
+    logging.info(f"Downloading data from {source} to {destination}")
+    create_directories([destination])
+    for file in tqdm(os.listdir(source)):
+        shutil.copy(os.path.join(source, file), destination)
+        logging.info(f"Downloaded data from {source} to {destination}")
